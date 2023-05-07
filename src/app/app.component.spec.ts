8b6b292f3df9c5eacb90d20f0e1590ffdea055dc
Willian Amancio <wvsamancio@gmail.com>
2023-05-07 20:18:30 -0300

    Changed app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-frontend2'`, () => {
+  it(`should have as title 'tsi3-front-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app-frontend2');
+    expect(app.title).toEqual('tsi3-front-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-frontend2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tsi3-front-end app is running!');
   });
 });
